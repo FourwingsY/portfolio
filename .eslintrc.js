b211c8de8a1c037ae79a7dace7620e0495475f94
HS Yang <fourwingsy@gmail.com>
2021-05-29 00:55:17 +0900

    chore: add eslint custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,24 @@
 module.exports = {
   root: true,
+  env: { browser: true, node: true },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  rules: {
+    // this forces exported functions to explicit return type
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+
+    // no unused variables with some exceptions
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { args: "after-used", ignoreRestSiblings: true, varsIgnorePattern: "^_" },
+    ],
+  },
+  overrides: [
+    // disable for .tsx files, but enbale for .ts files
+    {
+      rules: { "@typescript-eslint/explicit-module-boundary-types": ["error"] },
+      files: ["*.ts"],
+    },
+  ],
 }
