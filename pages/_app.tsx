c9f35e6ce42c4cebe67cfed2748018192f949c56
HS Yang <fourwingsy@gmail.com>
2021-05-29 01:30:17 +0900

    init: styled-components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import type { AppProps } from "next/app"
 
 import DefaultHead from "@components/Head/Default"
 
-import "../styles/globals.css"
+import GlobalStyle from "@styles/global"
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <GlobalStyle />
       <DefaultHead />
       <Component {...pageProps} />
     </>
