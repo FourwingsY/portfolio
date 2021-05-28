06443a4125b8402752326bc32854b5206d6aaf0d
HS Yang <fourwingsy@gmail.com>
2021-05-29 02:12:04 +0900

    feat: add example for responsive style

--- src/pages/Main/Main.style.ts
+++ src/pages/Main/Main.style.ts
@@ -1,6 +1,22 @@
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 export const Title = styled.h1``
 
-export const ScreenTypeChecker = styled.div``
+export const ScreenTypeChecker = styled.div`
+  ${({ theme }) =>
+    theme.large &&
+    css`
+      font-size: 16px;
+    `};
+  ${({ theme }) =>
+    theme.small &&
+    css`
+      font-size: 14px;
+    `};
+  ${({ theme }) =>
+    theme.tooSmall &&
+    css`
+      font-size: 12px;
+    `};
+`
 export const ScreenType = styled.p``
