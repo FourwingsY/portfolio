3c37fbb4fa157ea81359cab6eccfdb718e0b6bd6
HS Yang <fourwingsy@gmail.com>
2021-06-14 01:11:56 +0900

    fix: build error

--- pages/showcase/[post].ts
+++ pages/showcase/[post].ts
@@ -2,9 +2,11 @@ import { GetStaticPaths, GetStaticProps } from "next"
 
 export { default } from "@pages/Showcase/Post"
 
+const PROTOCOL = process.env.NODE_ENV === "development" ? "http" : "https"
+
 export const getStaticProps: GetStaticProps<{ post?: string }, { post: string }> = async (ctx) => {
   const postKey = ctx.params?.post
-  const post = await fetch(`${process.env.VERCEL_URL}/api/post/${postKey}`).then((res) => res.json())
+  const post = await fetch(`${PROTOCOL}://${process.env.VERCEL_URL}/api/post/${postKey}`).then((res) => res.json())
   return { props: { post: post } }
 }
 
--- pages/showcase/index.ts
+++ pages/showcase/index.ts
@@ -2,7 +2,9 @@ import { GetStaticProps } from "next"
 
 export { default } from "@pages/Showcase"
 
+const PROTOCOL = process.env.NODE_ENV === "development" ? "http" : "https"
+
 export const getStaticProps: GetStaticProps = async () => {
-  const posts = await fetch(`${process.env.VERCEL_URL}/api/posts`).then((res) => res.json())
+  const posts = await fetch(`${PROTOCOL}://${process.env.VERCEL_URL}/api/posts`).then((res) => res.json())
   return { props: { posts } }
 }
