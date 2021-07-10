b3a7457411e472afca9b8ecc9c1a26b07c1c66c9
HS Yang <fourwingsy@gmail.com>
2021-07-10 17:09:28 +0900

    feat: restart mole game on screen resize

--- src/pages/Main/sections/AboutMe/AboutMe.tsx
+++ src/pages/Main/sections/AboutMe/AboutMe.tsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react"
+
 import { TASTES } from "@constants/tastes"
+import { throttle } from "@utils/event"
 
 import * as S from "./AboutMe.style"
 import Mole from "./components/Mole"
 import TasteIcon from "./components/TasteIcon"
 
 const AboutMe = () => {
+  const [key, setKey] = useState(Date.now())
+
+  function resetMoleGame() {
+    setKey(Date.now())
+  }
+
+  // restart mole game when screen resizes
+  useEffect(() => {
+    const reset = throttle(resetMoleGame, 200)
+    window.addEventListener("resize", reset)
+    return () => window.removeEventListener("resize", reset)
+  }, [])
+
   return (
     <S.AboutMe>
       <S.Title>Who am I?</S.Title>
-      <S.MoleGame>
+      <S.MoleGame key={key}>
         {TASTES.map((taste) => (
           <Mole key={taste.name} taste={taste}>
             <TasteIcon taste={taste} />
