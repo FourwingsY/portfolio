b32b521d15ef4ce06e5d10fda055ff2538d63aad
HS Yang <fourwingsy@gmail.com>
2021-07-14 21:22:43 +0900

    fix: modal typing

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -1,4 +1,4 @@
-import { PayloadActionCreator } from "@reduxjs/toolkit"
+import { ActionCreator } from "@reduxjs/toolkit"
 
 import { AsyncAction } from "./async"
 import asyncActions from "./async/async.actions"
@@ -6,12 +6,12 @@ import modalActions from "./modal/modal.actions"
 
 export type DefinedActions = RootActionType<typeof modalActions> | RootActionType<typeof asyncActions>
 
-type RootActionType<RootAction extends Record<string, PayloadActionCreator<any> | AsyncAction<any, any>>> = {
+type RootActionType<RootAction extends Record<string, ActionCreator<any> | AsyncAction<any, any>>> = {
   [K in keyof RootAction]: ActionType<RootAction[K]>
 }[keyof RootAction]
 
-type ActionType<Action extends any> = Action extends PayloadActionCreator<any>
-  ? ReturnType<Action>
-  : Action extends AsyncAction<any, any>
+type ActionType<Action extends any> = Action extends AsyncAction<any, any>
   ? ReturnType<Action["request"] | Action["success"] | Action["failure"]>
+  : Action extends ActionCreator<any>
+  ? ReturnType<Action>
   : never
--- src/store/modal/modal.actions.ts
+++ src/store/modal/modal.actions.ts
@@ -1,4 +1,4 @@
-import { createAction } from "@reduxjs/toolkit"
+import { createAction, PayloadAction } from "@reduxjs/toolkit"
 
 import { ModalType } from "@modals/types"
 
@@ -13,7 +13,13 @@ function enhanceModalPayload<T extends ModalType>(payload: OpenModalPayload<T>):
   }
 }
 
-const openModal = createAction("@modal/OPEN_MODAL", enhanceModalPayload)
+// openModal 함수 자체가 generic function이어야
+// modalType: T 에 대한 props의 타입 추론이 가능하다
+type OpenModal = <T extends ModalType>(
+  payload: OpenModalPayload<T>
+) => PayloadAction<OpenModalPayload<T>, "@modal/OPEN_MODAL">
+
+const openModal = createAction("@modal/OPEN_MODAL", enhanceModalPayload) as OpenModal
 const closeModal = createAction<{ id: string }, "@modal/CLOSE_MODAL">("@modal/CLOSE_MODAL")
 const closeAll = createAction("@modal/CLOSE_ALL")
 
