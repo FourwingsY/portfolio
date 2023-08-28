d462c7bb470dd57b8282349007ebda38ee43aa05
YG <yg@medility.ai>
2023-08-28 21:46:33 +0900

    fix: table with all cell

--- src/pages/ComparePills/CompareTable.tsx
+++ src/pages/ComparePills/CompareTable.tsx
@@ -18,15 +18,11 @@ export default function CompareTable({ dataset, compareResults, onSelect }: Prop
       {dataset.map((set1, i) => (
         <tr key={i}>
           <th>image {i + 1}</th>
-          {dataset.map((set2, j) =>
-            i < j ? (
-              <td key={j} onClick={() => onSelect(set1.id, set2.id)}>
-                {compareResults[`${set1.id}-${set2.id}`]?.toFixed(2) ?? "비교하기"}
-              </td>
-            ) : (
-              <td />
-            )
-          )}
+          {dataset.map((set2, j) => (
+            <td key={j} onClick={() => onSelect(set1.id, set2.id)}>
+              {compareResults[`${set1.id}-${set2.id}`]?.toFixed(2) ?? "비교하기"}
+            </td>
+          ))}
         </tr>
       ))}
     </S.CompareTable>
